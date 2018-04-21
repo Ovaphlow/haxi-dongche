d94d1ee77190b00d9101e44b89bde6c0a5cec85d
ovaphlow <ovaphlow@live.com>
2018-04-21 16:31:48 +0800

    .

--- webpack/src/journal.index.js
+++ webpack/src/journal.index.js
@@ -3,7 +3,7 @@ import sidebar from './journal.sidebar.html'
 
 const user = JSON.parse(sessionStorage.getItem('auth'))
 
-if (!!!user) location.href = '../login.html'
+if (!!!user) location.href = './login.html'
 
 document.getElementById('navbar').innerHTML = navbar
 document.getElementById('sidebar').innerHTML = sidebar
