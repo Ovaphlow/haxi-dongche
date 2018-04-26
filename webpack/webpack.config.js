da8c12416ac679903ee96caa3182b08c12f4fd82
ovaphlow <ovaphlow@live.com>
2018-04-26 09:16:26 +0800

    .

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -6,10 +6,13 @@ module.exports = {
   entry: {
     'login': './src/login.js',
     'journal.index': './src/journal.index.js',
+
     'journal.01': './src/journal.01.js',
     'journal.01-save': './src/journal.01-save.js',
     'journal.01-borrow': './src/journal.01-borrow.js',
-    'journal.01-return': './src/journal.01-return.js'
+    'journal.01-return': './src/journal.01-return.js',
+
+    'journal.02': './src/journal.02.js'
   },
 
   devtool: 'inline-source-map',
