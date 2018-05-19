f0e7245abf3a5ed05eeb97c3dcba09ec6caaac68
ovaphlow <ovaphlow@live.com>
2018-05-19 14:43:13 +0800

    .

--- webpack/src/journal.02-detail.js
+++ webpack/src/journal.02-detail.js
@@ -10,7 +10,9 @@ document.getElementById('toolbar').innerHTML = toolbar
 let app = new Vue({
   el: '#app',
   data: {
-    journal: {}
+    journal: {},
+    detailList: [],
+    detail: {}
   },
   methods: {},
   created: function () {
@@ -23,11 +25,14 @@ let app = new Vue({
       if (response.data.content.tag === '一般部件普查记录单') {
         axios({
           method: 'GET',
-          url: './api/journal02/' + response.data.contnet.id + '/01/',
+          url: './api/journal02/' + response.data.content.id + '/01/',
           responseType: 'json'
         }).then(response => {
-          console.log(this.journal.tag)
-          console.log(response.data)
+          this.detailList = response.data.content
+          this.detail.subject = response.data.content[0].subject
+          this.detail.approval_sn = response.data.content[0].approval_sn
+          this.detail.train_sn = response.data.content[0].train_sn
+          this.detail.date = response.data.content[0].date
         })
       } else if (response.data.content.tag === '一般配件更换记录表') {
         axios({
@@ -35,8 +40,7 @@ let app = new Vue({
           url: './api/journal02/' + response.data.content.id + '/02/',
           responseType: 'json'
         }).then(response => {
-          console.log(this.journal.tag)
-          console.log(response.data)
+          this.detailList = response.data.content
         })
       } else if (response.data.content.tag === '关键配件更换记录表') {
         axios({
@@ -44,8 +48,7 @@ let app = new Vue({
           url: './api/journal02/' + response.data.content.id + '/03/',
           responseType: 'json'
         }).then(response => {
-          console.log(this.journal.tag)
-          console.log(response.data)
+          this.detailList = response.data.content
         })
       } else if (response.data.content.tag === '加装改造（软件升级）记录单') {
         axios({
@@ -53,8 +56,8 @@ let app = new Vue({
           url: './api/journal02/' + response.data.content.id + '/04/',
           responseType: 'json'
         }).then(response => {
-          console.log(this.journal.tag)
-          console.log(response.data)
+          this.detailList = response.data.content
+          this.detail.subject = response.data.content[0].subject
         })
       } else { return false }
     })
