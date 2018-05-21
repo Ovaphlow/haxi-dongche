065b49957333b8268f06927d2be42a29ee396c71
ovaphlow <ovaphlow@live.com>
2018-05-21 08:45:35 +0800

    .

--- node/app.js
+++ node/app.js
@@ -13,19 +13,19 @@ logger.level = config.app.logLevel
 
 app.set('env', config.app.env)
 
-let staticPath = path.join(__dirname, '../webpack/dist')
-app.use(express.static(staticPath))
-logger.info(`静态目录：${staticPath}`)
+// let staticPath = path.join(__dirname, '../webpack/dist')
+// app.use(express.static(staticPath))
+// logger.info(`静态目录：${staticPath}`)
 
-app.use('/lib', express.static(path.join(__dirname, 'node_modules')))
+// app.use('/lib', express.static(path.join(__dirname, 'node_modules')))
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json)
 
-app.use('/api', (req, res, next) => {
-  logger.info(req.method, req.originalUrl)
-  next()
-})
+// app.use('/api', (req, res, next) => {
+//   logger.info(req.method, req.originalUrl)
+//   next()
+// })
 
 app.use('/api/test', (req, res) => {
   logger.info(1123)
@@ -36,5 +36,5 @@ const user = require('./routes/user')
 app.use('/api/user', user)
 
 app.listen(config.app.port, () => {
-  logger.info(`服务器启动，端口 ${config.app.port}`)
+  logger.info(`服务器启动于端口 ${config.app.port}。`)
 })
